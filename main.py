002de389ff894a79ae6ffb3767861813b8ddd4ec
Ana Greer <cgreer24@biomedscienceacademy.org>
2020-11-10 13:59:13 +0000

    Added in variables

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Ana Greer
+Period/Core: 5
 
 
+ 
 """
+print("Let's play Silly Sentences!")
+ 
+name= input("What should her name be?: ")
+age= input("How old is she?: ")
+home= input("Where does she live?: ")
+
+print(name+" " "is" " " +age+ " " "years old. She lives in place, right next to the Four Seasons hotel. Her mother and father are some of the cooks there." +name+ "likes to spend her summers there, finding all of the hidden rooms and alleys in" " " +home+ " " ". She loves to get small treats and walk along the water or through the parks, taking in the towering buildings. Her uncle owns some of these buildings and always tries to talk her into working at them. She knows she will someday, maybe she'll even be in charge there, but for now, she wants to enjoy the cool breeze, the small shops, and the fast cars that fly down the streets each day.")
+print("For now, that is what" " " +name+ " " "wants, for now that is what brings her the most joy.")
